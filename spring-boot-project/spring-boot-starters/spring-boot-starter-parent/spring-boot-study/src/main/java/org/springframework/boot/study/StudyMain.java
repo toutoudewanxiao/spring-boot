c7581b53d702e851e451fd0cbd86183f054814a9
偷偷的玩笑 <630157610@qq.com>
2022-03-16 21:46:04 +0800

    新建study项目

---
+++ spring-boot-project/spring-boot-starters/spring-boot-starter-parent/spring-boot-study/src/main/java/org/springframework/boot/study/StudyMain.java
@@ -0,0 +1,16 @@
+package org.springframework.boot.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author huxiaohua
+ * @Date 2022/3/16 21:39
+ * @Version 1.0
+ */
+@SpringBootApplication
+public class StudyMain {
+	public static void main(String[] args) {
+		SpringApplication.run(StudyMain.class,args);
+	}
+}
